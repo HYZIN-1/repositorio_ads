082324e0b5c43297f6b4b8c6ba6f142eb0c9799e
giovanne <giovanneh.araujo05@gmail.com>
2025-04-14 19:22:13 -0300

    divisao

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
 Print("Isso é uma calculadora")
+
+
+
+
+print('informe os números abaixo')
+n1 = int(input('informe um número inteiro:'))
+n2 = int(input('informe um número inteiro:'))
+divisao = n1 / n2
+print(f'a divisão entre {n1} e {n2} é de {divisao}.')
--- divisao.py
+++
@@ -1,7 +0,0 @@
-print('''calcule a divisão entre dois números inteiros, para isso digite 
-      os números nos campos abaixo.''')
-n1 = int(input('número 1:')
-n2 = int(input('número 2:')
-divisao = n1/n2
-print(f'a divisão entre {n1} e {n2} é {divisao}')
-
